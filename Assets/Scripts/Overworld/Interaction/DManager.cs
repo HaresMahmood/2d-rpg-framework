78e2337c3965c4a1724b1b0fb630d933cddf135d
Hares Mahmood <hmahmood229@gmail.com>
2019-07-30 14:45:13 +0100

    Re-wrote and added functionality to Dialog-system.
    Dialog-system:
    - Implemented a different, less performance impacting system for storing dialog and text.
    - Pressing the "Interact"-button while text is typing will display the whole piece of dialog at once, similar to how dialog works in "Breath of the Wild".

---
+++ Assets/Scripts/Overworld/Interaction/DManager.cs
@@ -0,0 +1,98 @@
+﻿//TODO: 
+//- Change variable names.
+//- Look into animations by watching
+//Brackey's video.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DManager : MonoBehaviour //TODO: Change class name to "DialogManager"
+{
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    public GameObject dBox;
+
+    public bool isActive;
+    public bool isTyping;
+
+    public Animator animator;
+
+    public float waitTime = 0.02f;
+
+    private Queue<string> sentences;
+
+    // Use this for initialization
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialog dialogue)
+    {
+        //animator.SetBool("IsOpen", true);
+
+        dBox.SetActive(true);
+        isActive = true;
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        isTyping = true;
+
+        dialogueText.text = ""; //TODO: Turn into function called "ResetText()".
+
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+
+            yield return new WaitForSeconds(waitTime);
+
+            if (Input.GetButtonDown("Interact") && isTyping)
+            {
+                dialogueText.text = "";
+                dialogueText.text = sentence;
+
+                break;
+            }
+        }
+
+        isTyping = false;
+    }
+
+    void EndDialogue()
+    {
+        //animator.SetBool("IsOpen", false);
+        dBox.SetActive(false);
+        isActive = false;
+    }
+
+}
---
+++ Assets/Scripts/Overworld/Interaction/IOBject.cs
@@ -0,0 +1,44 @@
+﻿//TODO: Change variable names.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IOBject : MonoBehaviour //TODO: Change class name to "InteractableObject".
+{
+    private DManager dialogManager;
+
+
+    public Dialog dialogue;
+
+    void Start()
+    {
+        dialogManager = FindObjectOfType<DManager>();
+    }
+
+    void Update()
+    {
+        //TODO: Put "!dialogManager.isTyping" in if-statements.
+
+        if (Input.GetButtonDown("Interact") && !dialogManager.isActive)
+        {
+            if (!dialogManager.isTyping)
+            {
+                TriggerDialogue();
+            }
+        }
+        else if (Input.GetButtonDown("Interact") && dialogManager.isActive)
+        {
+            if (!dialogManager.isTyping)
+            {
+                dialogManager.DisplayNextSentence();
+            }
+        }
+    }
+
+    public void TriggerDialogue()
+    {
+        dialogManager.StartDialogue(dialogue);
+    }
+
+}
