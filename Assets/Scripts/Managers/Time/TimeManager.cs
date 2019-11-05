52bc17b39b72c2dbbab0b24c1489c01833ac07d6
Hares Mahmood <hmahmood229@gmail.com>
2019-11-05 23:19:55 +0000

    Created base class "TimeManager" to keep track of time globally.

---
+++ Assets/Scripts/Managers/Time/TimeManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///
+/// </summary>
+public class TimeManager : MonoBehaviour
+{
+    #region Variables
+
+    public const float realSecondsPerDays = 1420f; //1420f
+    public const float hoursPerDay = 24f;
+    public const float minutesPerHours = 60f;
+
+    [SerializeField] private Format format;
+
+    private float day;
+    private float hours;
+    private float minutes;
+
+    public enum Format
+    {
+        Twentyfour = 24,
+        Twelve = 12
+    }
+
+    #region Accessor Methods
+
+    public float GetDays()
+    {
+        return day;
+    }
+
+    public float GetDaysNormalized()
+    {
+        return day % 1f;
+    }
+
+    public (float hours, float minutes) GetTime()
+    {
+        return (hours: hours, minutes: minutes);
+    }
+
+    public Format GetFormat()
+    {
+        return format;
+    }
+
+    #endregion
+
+    #region Helper Methods
+
+    public void ConvertTime()
+    {
+        switch (format)
+        {
+            default: { break; };
+            case Format.Twelve:
+                {
+                    hours = format.ToString() == "AM" ? hours : (hours % (hoursPerDay / 2)) + (hoursPerDay / 2);
+                    break;
+                }
+            case Format.Twentyfour:
+                {
+                    // TODO: Not correct conversion!
+                    hours = Mathf.Clamp(hours, 0, (hoursPerDay / 2));
+                    hours = Mathf.Floor(hours /= 2);
+                    break;
+                }
+        }
+    }
+
+    #endregion
+
+    #endregion
+
+    #region Unity Methods
+
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
+    protected virtual void Update()
+    {
+        Format currentFormat = GetFormat();
+
+        day += Time.unscaledDeltaTime / realSecondsPerDays;
+
+        if (currentFormat != format)
+        {
+            currentFormat = format;
+            ConvertTime();
+        }
+
+        hours = Mathf.Floor((GetDaysNormalized() * (int)format));
+        minutes = Mathf.Floor((((GetDaysNormalized() * (int)format) % 1f)) * minutesPerHours);
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/UI/Pause/TimeUserInterface.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+///
+/// </summary>
+public class TimeUserInterface : TimeManager
+{
+    #region Variables
+
+    private TextMeshProUGUI clockText;
+
+    private Period period;
+
+    private bool isDirty;
+
+    private enum Period
+    {
+        AM,
+        PM
+    }
+
+    #endregion
+
+    #region Helper Methods
+
+    private IEnumerator IncrementPeriod(Period currentPeriod)
+    {
+        int newPeriod = ExtensionMethods.IncrementCircularInt((int)currentPeriod, 2, 1);
+        period = (Period)newPeriod;
+        yield return new WaitForSecondsRealtime(2f);
+        isDirty = false;
+    }
+
+    #endregion
+
+    #region Unity Methods
+
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
+    private void Start()
+    {
+        clockText = transform.Find("Value").GetComponent<TextMeshProUGUI>();
+    }
+
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
+    protected override void Update()
+    {
+        (float hours, float minutes) = GetTime();
+
+        // Debug
+        if (GetFormat() == Format.Twelve)
+        {
+            if (!isDirty && (hours == 0 && minutes == 0))
+            {
+                isDirty = true;
+                StartCoroutine(IncrementPeriod(period));
+            }
+            clockText.SetText($"{hours.ToString("00")}:{minutes.ToString("00")} <color=#696969>{period.ToString()}</color>");
+        }
+        else
+        {
+            clockText.SetText($"{hours}:{minutes}");
+        }
+
+        base.Update();
+    }
+
+    #endregion
+}
