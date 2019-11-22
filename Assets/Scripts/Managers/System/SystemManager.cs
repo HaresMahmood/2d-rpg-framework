00bc399b11f79b1b3e4244572494fb9cd859e2f8
Hares Mahmood <hmahmood229@gmail.com>
2019-11-22 17:05:26 +0000

    General improvements to setting menu.

--- Assets/Scripts/Managers/System/SystemManager.cs
+++ Assets/Scripts/Managers/System/SystemManager.cs
@@ -15,33 +15,22 @@ public class SystemManager : MonoBehaviour
 
     public static SystemManager instance;
 
-    [UnityEngine.Header("Setup (debug)")]
-    // Debug
-    public GameObject indicator;
-    public GameObject generalSettings;
-    public Scrollbar scrollBar;
-    public int selectedSetting, totalSettingOptions, totalSettingValues, selectedSettingValue;
-    public TextMeshProUGUI descriptionText;
 
-    [UnityEngine.Header("Settings")]
-    [SerializeField] private Material chartMaterial;
+    [Header("Values")]
+    public ViewingMode viewingMode; //{ get; private set; }
+    public bool isActive { get; set; } = false;
 
-    [UnityEngine.Header("Values")]
-    [SerializeField] private ViewingMode viewingMode;
+    [HideInInspector]
+    public GameObject systemContainer
+    {
+        get;
+        private set;
+    }
 
-    [HideInInspector] public GameObject systemContainer, navContainer;//, indicator; // Debug
-
-    private Transform[] navOptions, originalSettings, settings;
-    private string[] highlevelText = new string[] { "Save", "Settings", "Tutorials", "Controls", "Quit" };
-    private string[] settingsText = new string[] { "General", "Battle", "Customization", "Accessability", "Test" };
-
-
-    [HideInInspector] public int selectedNavOption, totalNavOptions;
-
-    [HideInInspector] public bool isActive = false, isDrawing = false, isInSettings = false, isSettingSelected = false;
-
-    public event EventHandler OnSettingSelected = delegate { };
-    private TestInput input = new TestInput();
+    [HideInInspector]
+    public string[] highlevelText { get; private set; } = new string[] { "Save", "Settings", "Tutorials", "Controls", "Quit" };
+    [HideInInspector] 
+    public string[] settingsText { get; private set; } = new string[] { "General", "Battle", "Customization", "Accessability", "Test" };
 
     public enum ViewingMode
     {
@@ -69,18 +58,6 @@ public class SystemManager : MonoBehaviour
     private void Start()
     {
         systemContainer = PauseManager.instance.pauseContainer.transform.Find("System").gameObject;
-        navContainer = systemContainer.transform.Find("Navigation").gameObject;
-
-        navOptions = navContainer.transform.Find("Options").GetChildren();
-
-        originalSettings = generalSettings.transform.GetChildren();
-        originalSettings = originalSettings.Where(val => val.name != "Text").ToArray();
-        settings = originalSettings;
-
-        selectedNavOption = 0;
-        ToggleViewingMode(viewingMode);
-        indicator.SetActive(false);
-        AnimateNavigation();
     }
 
     /// <summary>
@@ -88,202 +65,8 @@ public class SystemManager : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        if (PauseManager.instance.isPaused)
-        {
-            GetInput();
-        }
+
     }
 
     #endregion
-
-    private void AnimateNavigation()
-    {
-        foreach (Transform option in navOptions)
-        {
-            if (Array.IndexOf(navOptions, option) == selectedNavOption)
-            {
-                option.GetComponent<Animator>().SetBool("isSelected", true);
-                StartCoroutine(option.GetComponentInChildren<TextMeshProUGUI>().gameObject.FadeColor(GameManager.GetAccentColor(), 0.1f));
-            }
-            else
-            {
-                option.GetComponent<Animator>().SetBool("isSelected", false);
-                StartCoroutine(option.GetComponentInChildren<TextMeshProUGUI>().gameObject.FadeColor(Color.white, 0.1f));
-            }
-        }
-    }
-
-    private IEnumerator AnimateOptions()
-    {
-        if (!isInSettings)
-        {
-            navContainer.transform.Find("Options").GetComponent<Animator>().SetBool("isInHighlevel", false);
-            navContainer.transform.Find("Options").GetComponent<Animator>().SetBool("isInSettings", true);
-            StartCoroutine(generalSettings.transform.parent.gameObject.FadeOpacity(0.5f, 0.3f));
-            StartCoroutine(descriptionText.gameObject.FadeOpacity(1f, 0.3f));
-            isInSettings = true;
-        }
-        else
-        {
-            isInSettings = false;
-            navContainer.transform.Find("Options").GetComponent<Animator>().SetBool("isInHighlevel", true);
-            navContainer.transform.Find("Options").GetComponent<Animator>().SetBool("isInSettings", false);
-            StartCoroutine(generalSettings.transform.parent.gameObject.FadeOpacity(0f, 0.3f));
-            StartCoroutine(descriptionText.gameObject.FadeOpacity(0f, 0.3f));
-        }
-
-        yield return new WaitForSecondsRealtime(navContainer.transform.Find("Options").GetComponent<Animator>().GetAnimationTime() / 2);
-        selectedNavOption = 0;
-        AnimateNavigation();
-        DrawSettings();
-    }
-
-    private void DrawSettings()
-    {
-        string[] newText;
-        if (isInSettings)
-        {
-            newText = settingsText;
-        }
-        else
-        {
-            newText = highlevelText;
-        }
-
-        foreach (Transform option in navOptions)
-        {
-            option.GetComponentInChildren<TextMeshProUGUI>().SetText(newText[Array.IndexOf(navOptions, option)]);
-        }
-    }
-
-    private void GetInput()
-    {
-        if (!PauseManager.instance.inPartyMenu && isActive)
-        {
-            if (!isSettingSelected)
-            {
-                totalNavOptions = navOptions.Length; // Debug
-
-                bool hasInput;
-                (selectedNavOption, hasInput) = input.GetInput("Vertical", TestInput.Axis.Vertical, totalNavOptions, selectedNavOption);
-                if (hasInput)
-                {
-                    input.OnUserInput += PartyManager_OnUserInput;
-                }
-                if (Input.GetButtonDown("Interact"))
-                {
-                    if (!isInSettings)
-                    {
-                        StartCoroutine(AnimateOptions());
-                    }
-                    else
-                    {
-                        indicator.SetActive(true);
-                        StartCoroutine(generalSettings.transform.parent.gameObject.FadeOpacity(1f, 0.3f));
-                        isSettingSelected = true;
-                    }
-                }
-                if (Input.GetButtonDown("Cancel") && isInSettings)
-                {
-                    StartCoroutine(AnimateOptions());
-                }
-            }
-            else
-            {
-                bool hasInput;
-                (selectedSetting, hasInput) = input.GetInput("Vertical", TestInput.Axis.Vertical, totalSettingOptions, selectedSetting);
-                if (hasInput)
-                {
-                    input.OnUserInput += PartyManager_OnUserInput;
-                }
-                if (Input.GetButtonDown("Toggle"))
-                {
-                    viewingMode = (ViewingMode)ExtensionMethods.IncrementCircularInt((int)viewingMode, Enum.GetNames(typeof(ViewingMode)).Length, 1);
-                    ToggleViewingMode(viewingMode);
-                    UpdateSettingCategory();
-                }
-                if (Input.GetButtonDown("Cancel"))
-                {
-                    indicator.SetActive(false);
-                    StartCoroutine(generalSettings.transform.parent.gameObject.FadeOpacity(0.5f, 0.3f));
-                    isSettingSelected = false;
-                }
-            }
-        }
-    }
-
-    private void ToggleViewingMode(ViewingMode mode)
-    {
-        settings = originalSettings;
-        List<ViewingMode> previousMode = GetPreviousMode(mode);
-        List<Transform> values = new List<Transform>();
-
-        foreach (Transform setting in settings)
-        {
-            if (previousMode.Contains(setting.GetComponent<SettingValue>().GetViewingMode()))
-            {
-                values.Add(setting);
-                setting.gameObject.SetActive(true);
-            }
-            else
-            {
-                setting.gameObject.SetActive(false);
-            }
-        }
-
-        settings = values.ToArray();
-        selectedSettingValue = 0;
-        scrollBar.value = 1;
-    }
-
-    private void UpdateSettingCategory()
-    {
-        float settingTotal = (float)settings.Length;
-        float targetValue = 1.0f - (float)selectedSetting / (settingTotal - 1);
-        StartCoroutine(scrollBar.LerpScrollbar(targetValue, 0.08f));
-
-        totalSettingOptions = settings.Length;
-        if (settings[selectedSetting].childCount > 0)
-        {
-            indicator.transform.position = settings[selectedSetting].Find("Value").position;
-        }
-
-        totalSettingValues = settings[selectedSetting].GetComponent<SettingValue>().GetValues().Count;
-        selectedSettingValue = settings[selectedSetting].GetComponent<SettingValue>().GetValues().FindIndex(value => value == settings[selectedSetting].GetComponent<SettingValue>().GetSelectedValue());
-        foreach (Transform setting in settings)
-        {
-            if (Array.IndexOf(settings, setting) != selectedSettingValue)
-            {
-                setting.GetComponent<SettingValue>().SetStatus(false);
-            }
-        }
-        settings[selectedSetting].GetComponent<SettingValue>().SetStatus(true);
-
-        descriptionText.SetText(settings[selectedSetting].GetComponent<SettingValue>().GetDescription());
-    }
-
-    private List<ViewingMode> GetPreviousMode(ViewingMode category)
-    {
-        ViewingMode previousCategory = (ViewingMode)ExtensionMethods.IncrementCircularInt((int)category, Enum.GetNames(typeof(ViewingMode)).Length, -1);
-        List<ViewingMode> previousCategories = new List<ViewingMode>();
-        if (previousCategory < category)
-        {
-            previousCategories.AddRange(GetPreviousMode(previousCategory));
-        }
-        previousCategories.Add(category);
-        return previousCategories;
-    }
-
-    private void PartyManager_OnUserInput(object sender, EventArgs e)
-    {
-        if (!isSettingSelected)
-        {
-            AnimateNavigation();
-        }
-        else
-        { 
-            UpdateSettingCategory();
-        }
-        input.OnUserInput -= PartyManager_OnUserInput;
-    }
 }
---
+++ Assets/Scripts/Managers/System/SystemUserInterface.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+///
+/// </summary>
+public class SystemUserInterface : MonoBehaviour
+{
+    #region Variables
+
+    public event EventHandler OnSettingSelected = delegate { };
+
+    private TestInput input = new TestInput();
+
+    private GameObject navContainer, generalSettings, indicator;
+    private Transform[] navOptions, originalSettings, settings;
+    private Scrollbar scrollBar;
+    private TextMeshProUGUI descriptionText;
+
+    [HideInInspector] public int selectedNavOption, totalNavOptions;
+    private int selectedSetting, totalSettingOptions, totalSettingValues, selectedSettingValue;
+
+    //private bool isInSettings = false, isSettingSelected = false;
+    private Flags flags = new Flags(false, false);
+
+    private struct Flags
+    {
+        public bool isInSettings { get; set; }
+        public bool isSettingSelected { get; set; }
+        /*
+        {
+            get { return isSettingSelected; }
+            set { if (isSettingSelected) isSettingSelected = value; }
+        }
+        */
+
+        public Flags(bool isInSettings, bool isSettingSelected)
+        {
+            this.isInSettings = isInSettings;
+            this.isSettingSelected = isSettingSelected;
+        }
+    }
+
+    #endregion
+
+    #region Event Methods
+
+    private void SystemManager_OnUserInput(object sender, EventArgs e)
+    {
+        if (!flags.isSettingSelected)
+        {
+            AnimateNavigation();
+            foreach (Transform setting in settings)
+            {
+                setting.GetComponent<SettingValue>().SetStatus(false);
+            }
+        }
+        else
+        {
+            UpdateSettingCategory();
+        }
+        input.OnUserInput -= SystemManager_OnUserInput;
+    }
+
+    #endregion
+
+    #region Miscellaneous Methods
+
+    private List<SystemManager.ViewingMode> GetPreviousMode(SystemManager.ViewingMode category)
+    {
+        SystemManager.ViewingMode previousCategory = (SystemManager.ViewingMode)ExtensionMethods.IncrementCircularInt((int)category, Enum.GetNames(typeof(SystemManager.ViewingMode)).Length, -1);
+        List<SystemManager.ViewingMode> previousCategories = new List<SystemManager.ViewingMode>();
+        if (previousCategory < category)
+        {
+            previousCategories.AddRange(GetPreviousMode(previousCategory));
+        }
+        previousCategories.Add(category);
+        return previousCategories;
+    }
+
+    private void ToggleViewingMode(SystemManager.ViewingMode mode)
+    {
+        settings = originalSettings;
+        List<SystemManager.ViewingMode> previousMode = GetPreviousMode(mode);
+        List<Transform> values = new List<Transform>();
+
+        foreach (Transform setting in settings)
+        {
+            if (previousMode.Contains(setting.GetComponent<SettingValue>().GetViewingMode()))
+            {
+                values.Add(setting);
+                setting.gameObject.SetActive(true);
+            }
+            else
+            {
+                setting.gameObject.SetActive(false);
+            }
+        }
+
+        settings = values.ToArray();
+        selectedSettingValue = 0;
+        scrollBar.value = 1;
+    }
+
+    private void UpdateSettingCategory()
+    {
+        float settingTotal = (float)settings.Length;
+        float targetValue = 1.0f - (float)selectedSetting / (settingTotal - 1);
+        StartCoroutine(scrollBar.LerpScrollbar(targetValue, 0.08f));
+
+        totalSettingOptions = settings.Length;
+        if (settings[selectedSetting].childCount > 0)
+        {
+            indicator.transform.position = settings[selectedSetting].Find("Value").position;
+        }
+
+        totalSettingValues = settings[selectedSetting].GetComponent<SettingValue>().GetValues().Count;
+        selectedSettingValue = settings[selectedSetting].GetComponent<SettingValue>().GetValues().FindIndex(value => value == settings[selectedSetting].GetComponent<SettingValue>().GetSelectedValue());
+        foreach (Transform setting in settings)
+        {
+            if (Array.IndexOf(settings, setting) != selectedSettingValue)
+            {
+                setting.GetComponent<SettingValue>().SetStatus(false);
+            }
+        }
+        settings[selectedSetting].GetComponent<SettingValue>().SetStatus(true);
+
+        descriptionText.SetText(settings[selectedSetting].GetComponent<SettingValue>().GetDescription());
+    }
+
+    private void DrawSettings()
+    {
+        string[] newText;
+        if (flags.isInSettings)
+        {
+            newText = SystemManager.instance.settingsText;
+        }
+        else
+        {
+            newText = SystemManager.instance.highlevelText;
+        }
+
+        foreach (Transform option in navOptions)
+        {
+            option.GetComponentInChildren<TextMeshProUGUI>().SetText(newText[Array.IndexOf(navOptions, option)]);
+        }
+    }
+
+    private IEnumerator AnimateOptions()
+    {
+        if (!flags.isInSettings)
+        {
+            navContainer.transform.Find("Options").GetComponent<Animator>().SetBool("isInHighlevel", false);
+            navContainer.transform.Find("Options").GetComponent<Animator>().SetBool("isInSettings", true);
+            StartCoroutine(generalSettings.transform.parent.gameObject.FadeOpacity(0.5f, 0.3f));
+            StartCoroutine(descriptionText.gameObject.FadeOpacity(1f, 0.3f));
+            flags.isInSettings = true;
+        }
+        else
+        {
+            flags.isInSettings = false;
+            navContainer.transform.Find("Options").GetComponent<Animator>().SetBool("isInHighlevel", true);
+            navContainer.transform.Find("Options").GetComponent<Animator>().SetBool("isInSettings", false);
+            StartCoroutine(generalSettings.transform.parent.gameObject.FadeOpacity(0f, 0.3f));
+            StartCoroutine(descriptionText.gameObject.FadeOpacity(0f, 0.3f));
+        }
+
+        yield return new WaitForSecondsRealtime(navContainer.transform.Find("Options").GetComponent<Animator>().GetAnimationTime() / 2);
+        selectedNavOption = 0;
+        AnimateNavigation();
+        DrawSettings();
+    }
+
+    private void AnimateNavigation()
+    {
+        foreach (Transform option in navOptions)
+        {
+            if (Array.IndexOf(navOptions, option) == selectedNavOption)
+            {
+                option.GetComponent<Animator>().SetBool("isSelected", true);
+                StartCoroutine(option.GetComponentInChildren<TextMeshProUGUI>().gameObject.FadeColor(GameManager.GetAccentColor(), 0.1f));
+            }
+            else
+            {
+                option.GetComponent<Animator>().SetBool("isSelected", false);
+                StartCoroutine(option.GetComponentInChildren<TextMeshProUGUI>().gameObject.FadeColor(Color.white, 0.1f));
+            }
+        }
+    }
+
+    private void GetInput()
+    {
+        if (!PauseManager.instance.inPartyMenu && SystemManager.instance.isActive)
+        {
+            if (!flags.isSettingSelected)
+            {
+                bool hasInput;
+                (selectedNavOption, hasInput) = input.GetInput("Vertical", TestInput.Axis.Vertical, totalNavOptions, selectedNavOption);
+                if (hasInput)
+                {
+                    input.OnUserInput += SystemManager_OnUserInput;
+                }
+                if (Input.GetButtonDown("Interact"))
+                {
+                    if (!flags.isInSettings)
+                    {
+                        StartCoroutine(AnimateOptions());
+                    }
+                    else
+                    {
+                        indicator.SetActive(true);
+                        StartCoroutine(generalSettings.transform.parent.gameObject.FadeOpacity(1f, 0.3f));
+                        flags.isSettingSelected = true;
+                    }
+                }
+                if (Input.GetButtonDown("Cancel") && flags.isInSettings)
+                {
+                    StartCoroutine(AnimateOptions());
+                }
+            }
+            else
+            {
+                bool hasInput;
+                (selectedSetting, hasInput) = input.GetInput("Vertical", TestInput.Axis.Vertical, totalSettingOptions, selectedSetting);
+                if (hasInput)
+                {
+                    input.OnUserInput += SystemManager_OnUserInput;
+                }
+                if (Input.GetButtonDown("Toggle"))
+                {
+                    SystemManager.instance.viewingMode = (SystemManager.ViewingMode)ExtensionMethods.IncrementCircularInt((int)SystemManager.instance.viewingMode, Enum.GetNames(typeof(SystemManager.ViewingMode)).Length, 1);
+                    ToggleViewingMode(SystemManager.instance.viewingMode);
+                    UpdateSettingCategory();
+                }
+                if (Input.GetButtonDown("Cancel"))
+                {
+                    indicator.SetActive(false);
+                    StartCoroutine(generalSettings.transform.parent.gameObject.FadeOpacity(0.5f, 0.3f));
+                    flags.isSettingSelected = false;
+                }
+            }
+        }
+    }
+
+    #endregion
+
+    #region Unity Methods
+
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
+    private void Start()
+    {
+        navContainer = transform.Find("Navigation").gameObject;
+        generalSettings = transform.Find("Settings/General").gameObject;
+        indicator = transform.Find("Settings/Indicator").gameObject;
+
+        navOptions = navContainer.transform.Find("Options").GetChildren();
+
+        descriptionText = transform.Find("Description").GetComponent<TextMeshProUGUI>();
+        scrollBar = transform.Find("Settings/Scrollbar").GetComponent<Scrollbar>();
+
+        originalSettings = generalSettings.transform.GetChildren();
+        originalSettings = originalSettings.Where(val => val.name != "Text").ToArray();
+        settings = originalSettings;
+
+        totalNavOptions = navOptions.Length; // Debug
+
+        selectedNavOption = 0;
+        indicator.SetActive(false);
+        ToggleViewingMode(SystemManager.instance.viewingMode);
+        AnimateNavigation();
+    }
+
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
+    private void Update()
+    {
+        if (PauseManager.instance.isPaused)
+        {
+            GetInput();
+        }
+    }
+
+    #endregion
+}
