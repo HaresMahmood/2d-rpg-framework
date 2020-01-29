3a4e43cf3ff8f8bbe1a45656e7134244f487bcc0
haresm <hmahmood229@gmail.com>
2020-01-29 19:26:49 +0000

    Addition: add back-end to correctly display main information.
    Edits made to: mission menu.

---
+++ Assets/Scripts/Managers/Mission/MissionMainPanel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+///
+/// </summary>
+public class MissionMainPanel : MonoBehaviour
+{
+    #region Variables
+
+    private Transform panel;
+
+    private TextMeshProUGUI nameText;
+    private TextMeshProUGUI objectiveText;
+    private TextMeshProUGUI descriptionText;
+
+    #endregion
+
+    #region Miscellaneous Methods
+
+    public void UpdateInformation(Mission mission)
+    {
+        nameText.SetText(mission.Name);
+        objectiveText.SetText(mission.objective);
+        descriptionText.SetText(mission.description);
+    }
+
+    #endregion
+
+    #region Unity Methods
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake()
+    {
+        panel = transform.Find("Information");
+
+        nameText = panel.Find("Name").GetComponentInChildren<TextMeshProUGUI>();
+        objectiveText = panel.Find("Objective/Value").GetComponent<TextMeshProUGUI>();
+        descriptionText = panel.Find("Description/Value").GetComponent<TextMeshProUGUI>();
+    }
+
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
+    private void Update()
+    {
+        
+    }
+
+    #endregion
+}
--- Assets/Scripts/Managers/Mission/MissionUserInterface.cs
+++ Assets/Scripts/Managers/Mission/MissionUserInterface.cs
@@ -17,13 +17,33 @@ public class MissionUserInterface : MonoBehaviour
 
     private GameObject leftPanel;
     private GameObject rightPanel;
+    private GameObject indicator;
+
+    private Animator indicatorAnimator;
 
 
     #endregion
 
     #region Miscellaneous Methods
 
-    
+    /// <summary>
+    /// Animates and updates the position of the indicator. Dynamically changes position and size of indicator depending on what situation it is used for. If no value is selected, the indicator completely fades out.
+    /// </summary>
+    /// <param name="selectedValue"> Index of the value currently selected. Defaults to -1 (null/no value). </param>
+    /// <param name="duration"> Duration of the animation/fade. </param>
+    /// <param name="isInSubMenu"> Whether or not the indicator is used in the sub menu. Defaults to false. </param>
+    /// <returns> Co-routine. </returns>
+    public IEnumerator UpdateIndicator(int selectedValue, float duration = 0.1f)
+    {
+        indicatorAnimator.enabled = false;
+        StartCoroutine(indicator.FadeOpacity(0f, duration));
+        yield return new WaitForSecondsRealtime(duration);
+
+        indicator.transform.position = missionPanels[selectedValue].transform.position; 
+        
+        yield return null;
+        indicatorAnimator.enabled = true;
+    }
 
     #endregion
 
@@ -36,13 +56,19 @@ public class MissionUserInterface : MonoBehaviour
     {
         leftPanel = transform.Find("Left").gameObject;
         rightPanel = transform.Find("Right").gameObject;
+        indicator = leftPanel.transform.Find("List/Indicator").gameObject;
+
+        indicatorAnimator = indicator.GetComponent<Animator>();
 
         missionPanels = leftPanel.transform.Find("List/Mission List").GetComponentsInChildren<MissionPanel>();
 
+        rightPanel.GetComponentInChildren<MissionMainPanel>().UpdateInformation(MissionManager.instance.missions.mission[0]);
         for (int i = 0; i < MissionManager.instance.missions.mission.Count; i++)
         {
             missionPanels[i].UpdateInformation(MissionManager.instance.missions.mission[i]);
         }
+
+        StartCoroutine(UpdateIndicator(0));
     }
 
     /// <summary>
