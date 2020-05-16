edd4c0ac65ffb12e0294b35021c465781a492667
haresm <hmahmood229@gmail.com>
2020-05-16 10:45:02 +0100

    Modification: add majority of information of "Progression" information panel.
    "Met at location" field still not being edited.
    
    (Party/Information Panel/Progression)

--- Assets/Scripts/User Interface/Party/Slots/Information Slots/PartyProgressionSlot.cs
+++ Assets/Scripts/User Interface/Party/Slots/Information Slots/PartyProgressionSlot.cs
@@ -12,12 +12,16 @@ public class PartyProgressionSlot : PartyInformationSlot
 
     private Transform level;
     private Transform experience;
+    private Transform metIn;
 
     private TextMeshProUGUI levelText;
 
     private TextMeshProUGUI experienceText;
     private TextMeshProUGUI remainingExperienceText;
     private Slider experienceBar;
+
+    private TextMeshProUGUI metInLocationText;
+    private TextMeshProUGUI metAtLevelText;
 
     #endregion
 
@@ -31,10 +35,15 @@ public class PartyProgressionSlot : PartyInformationSlot
 
         experienceText.SetText(member.Progression.Value.ToString());
         remainingExperienceText.SetText((member.Progression.Level < 100 && member.Progression.Level > 0) ? member.Progression.GetRemaining(member.Species).ToString() : "-");
+        experienceText.GetComponent<AutoTextWidth>().UpdateWidth(member.Progression.Value.ToString());
+        remainingExperienceText.GetComponent<AutoTextWidth>().UpdateWidth((member.Progression.Level < 100 && member.Progression.Level > 0) ? member.Progression.GetRemaining(member.Species).ToString() : "-");
 
         double experience = member.Progression.Value, remaining = member.Progression.GetRemaining(member.Species);
         double result = experience / remaining;
         experienceBar.value = (float)result;
+
+        metAtLevelText.SetText(member.MetAt.Level.ToString());
+        // TODO: Add "met at location" text.
     }
 
     #endregion
@@ -50,12 +59,16 @@ public class PartyProgressionSlot : PartyInformationSlot
 
         level = slot.Find("Level");
         experience = slot.Find("Experience");
+        metIn = slot.Find("Met In");
 
         levelText = level.Find("Value").GetComponent<TextMeshProUGUI>();
 
         experienceText = experience.Find("Text/Values/Value").GetComponent<TextMeshProUGUI>();
         remainingExperienceText = experience.Find("Text/Values/Remaining/Value").GetComponent<TextMeshProUGUI>();
         experienceBar = experience.Find("Experience Bar").GetComponent<Slider>();
+
+        metInLocationText = metIn.Find("Location/Value").GetComponent<TextMeshProUGUI>();
+        metAtLevelText = metIn.Find("At Level/Value").GetComponent<TextMeshProUGUI>();
 
         base.Awake();
     }
