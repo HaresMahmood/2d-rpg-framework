4ffb9058511ca724e080a97bea02111f4e31e4ff
haresm <hmahmood229@gmail.com>
2020-05-21 10:46:08 +0100

    Addition: add "StatsUserInterface".
    (Party/Stats)

---
+++ Assets/Scripts/User Interface/Party/Stats/StatsUserInterface.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsUserInterface : MonoBehaviour
+{
+    #region Variables
+
+    private CanvasRenderer radarChartMesh;
+
+    #endregion
+
+    #region Miscellaneous Methodss
+
+    private void DrawStatChart()
+    {
+    }
+
+    #endregion
+
+    #region Unity Methods
+
+
+
+    #endregion
+}
