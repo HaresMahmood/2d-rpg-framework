a886ff9f3287d5cedd968ff38229f8e81432241f
haresm <hmahmood229@gmail.com>
2020-06-18 10:40:33 +0100

    Modification: remove unnecessary fields.
    Remove "isFailed" property, as it is unneeded.
    
    (Mission)

--- Assets/Scripts/Metadata/Categorization/Mission/Mission.cs
+++ Assets/Scripts/Metadata/Categorization/Mission/Mission.cs
@@ -12,7 +12,6 @@ public class Mission : Categorizable
     [SerializeField] private MissionOriginDestination originDestination = new MissionOriginDestination();
     [SerializeField] private List<MissionGoal> goals = new List<MissionGoal>();
     [SerializeField] private List<MissionReward> rewards = new List<MissionReward>();
-    [SerializeField] private bool isFailed;
 
     #endregion
 
