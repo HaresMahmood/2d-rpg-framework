49d82c0d9afa4f336054f96143b9d45bdda7584c
haresm <hmahmood229@gmail.com>
2020-07-18 17:18:51 +0100

    Modification: refactor "BranchingDialog" ScriptableObject.

--- Assets/Scripts/Overworld/Interaction/Dialog/ScriptableObjects/BranchingDialog.cs
+++ Assets/Scripts/Overworld/Interaction/Dialog/ScriptableObjects/BranchingDialog.cs
@@ -5,15 +5,64 @@ using UnityEngine.Events;
 [CreateAssetMenu(fileName = "New branch", menuName = "Dialog/Branch")]
 public class BranchingDialog : ScriptableObject
 {
+    #region Fields
+
+    [SerializeField] private List<DialogBranch> branches = new List<DialogBranch>();
+
+    #endregion
+
+    #region Properties
+
+    public List<DialogBranch> Branches
+    {
+        get { return branches; }
+    }
+
+    #endregion
+
+    #region Nested Classes
+
     [System.Serializable]
     public class DialogBranch
     {
-        public string branchOption;
-        public Dialog nextDialog;
-        public UnityEvent branchEvent;
-        public bool hasBackButton;
+        #region Fields
+
+        [SerializeField] private string text;
+        [SerializeField] private Dialog nextDialog;
+        [SerializeField] private UnityEvent branchEvent;
+        [SerializeField] private bool hasBackButton;
+
+        #endregion
+
+        #region Properties
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public Dialog NextDialog
+        {
+            get { return nextDialog; }
+            set { nextDialog = value; }
+        }
+
+        public UnityEvent BranchEvent
+        {
+            get { return branchEvent; }
+            set { branchEvent = value; }
+        }
+
+        public bool HasBackButton
+        {
+            get { return hasBackButton; }
+            set { hasBackButton = value; }
+        }
+
+        #endregion
     }
 
-    public List<DialogBranch> dialogBranches = new List<DialogBranch>();
+    #endregion
 }
 
