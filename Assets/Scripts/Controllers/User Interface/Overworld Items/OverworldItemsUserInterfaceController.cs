dbb1c0cfff28d021c5d2782aa0e596a6df5ed19c
haresm <hmahmood229@gmail.com>
2020-07-31 23:01:06 +0100

    Addition: add animations for overword item UI panels.

---
+++ Assets/Scripts/Controllers/User Interface/Overworld Items/OverworldItemsUserInterfaceController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///
+/// </summary>
+public class OverworldItemsUserInterfaceController : UserInterfaceController
+{
+    [InspectorButton("OnButtonClicked")]
+    public bool isActive;
+
+    #region Fields
+
+    private static OverworldItemsUserInterfaceController instance;
+
+    [SerializeField] private OverworldItemsUserInterface userInterface;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Singleton pattern.
+    /// </summary>
+    public static OverworldItemsUserInterfaceController Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<OverworldItemsUserInterfaceController>();
+            }
+
+            return instance;
+        }
+    }
+
+    public override UserInterface UserInterface
+    {
+        get { return userInterface; }
+    }
+
+    #endregion
+
+    #region Miscellaneous Methods
+
+    public override IEnumerator SetActive(bool isActive, bool condition = true)
+    {
+        if (isActive)
+        {
+            Debug.Log(true);
+
+            StartCoroutine(userInterface.SetActive(++selectedValue));
+
+            yield break;
+        }
+    }
+
+    private void OnButtonClicked()
+    {
+        StartCoroutine(SetActive(true));
+    }
+
+    #endregion
+}
+
---
+++ Assets/Scripts/User Interface/Overworld Items/OverworldItemsUserInterface.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///
+/// </summary>
+public class OverworldItemsUserInterface : UserInterface
+{
+    #region Constants
+
+    public override int MaxObjects => 0;
+
+    #endregion
+
+    #region Variables
+
+    List<OverworldItemPanel> panels;
+
+    #endregion
+
+    #region Miscellaneous Methods
+
+    public IEnumerator SetActive(int selectedValue)
+    {
+        panels[selectedValue].gameObject.SetActive(true);
+
+        yield return null;  yield return new WaitForSecondsRealtime(panels[selectedValue].GetComponent<Animator>().GetAnimationTime());
+
+        panels[selectedValue].gameObject.SetActive(false);
+    }
+
+    #endregion
+
+    #region Unity Methods
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    protected override void Awake()
+    {
+        panels = GetComponentsInChildren<OverworldItemPanel>().ToList();
+        panels.Reverse();
+
+        foreach (OverworldItemPanel panel in panels)
+        {
+            panel.gameObject.SetActive(false);
+        }
+    }
+
+    #endregion
+}
+
