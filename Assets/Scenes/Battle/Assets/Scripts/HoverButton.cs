b021c541066c63240d68c3b38696550d3cc4a066
Hares Mahmood <hmahmood229@gmail.com>
2020-09-02 11:06:00 +0100

    Make move buttons in Battle Scene interactable.

---
+++ Assets/Scenes/Battle/Assets/Scripts/HoverButton.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+/// <summary>
+///
+/// </summary>
+public class HoverButton : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    #region Variables
+
+
+
+    #endregion
+
+    #region Miscellaneous Methods
+
+
+
+    #endregion
+
+    #region Unity Methods
+
+    // When highlighted with mouse.
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.Find("Text").gameObject.SetActive(true);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponentInParent<RectTransform>());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.Find("Text").gameObject.SetActive(false);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponentInParent<RectTransform>());
+    }
+
+    // When selected.
+    public void OnSelect(BaseEventData eventData)
+    {
+        transform.Find("Text").gameObject.SetActive(true);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponentInParent<RectTransform>());
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        transform.Find("Text").gameObject.SetActive(false);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponentInParent<RectTransform>());
+    }
+
+    #endregion
+}
+
