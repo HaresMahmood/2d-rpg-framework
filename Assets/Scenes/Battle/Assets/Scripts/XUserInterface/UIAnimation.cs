474fc49fb52e9f5131e003e414be191872436b86
Hares Mahmood <hmahmood229@gmail.com>
2020-09-14 09:21:15 +0100

    Change types of variables in UIAnimation.

--- Assets/Scenes/Battle/Assets/Scripts/XUserInterface/UIAnimation.cs
+++ Assets/Scenes/Battle/Assets/Scripts/XUserInterface/UIAnimation.cs
@@ -16,19 +16,16 @@ public class UIAnimation : MonoBehaviour
     // TODO: Chang ename to reflect it's only used in BattleSystem UI
 
 
-    // TODO: Set all canvasgroups to rectransform!!*
-
-
 
     #region Variables
 
     [Header("Setup")]
-    [SerializeField] private CanvasGroup content; // *
+    [SerializeField] private RectTransform content; // *
 
     [Header("Settings")]
     [SerializeField, Range(0.01f, 2f)] private float animationTime = 0.3f;
 
-    private CanvasGroup background; // *
+    private RectTransform background; // *
 
     Sequence sequence;
 
@@ -45,31 +42,26 @@ public class UIAnimation : MonoBehaviour
             gameObject.SetActive(isActive);
             LayoutRebuilder.ForceRebuildLayoutImmediate(content.GetComponent<RectTransform>());
 
-            background.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
-            background.GetComponent<RectTransform>().anchoredPosition = new Vector2(background.GetComponent<RectTransform>().anchoredPosition.x, -75f);
-            background.GetComponent<RectTransform>().sizeDelta = new Vector2(background.GetComponent<RectTransform>().sizeDelta.x, 0f);
+            background.localScale = new Vector3(1f, 1f, 1f);
+            background.anchoredPosition = new Vector2(background.GetComponent<RectTransform>().anchoredPosition.x, -75f);
+            background.sizeDelta = new Vector2(background.GetComponent<RectTransform>().sizeDelta.x, 0f);
 
-            content.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
-            content.GetComponent<RectTransform>().anchoredPosition = new Vector2(background.GetComponent<RectTransform>().anchoredPosition.x, -250f);
-            content.GetComponent<RectTransform>().sizeDelta = new Vector2(background.GetComponent<RectTransform>().sizeDelta.x, 0f);
+            content.localScale = new Vector3(1f, 1f, 1f);
+            content.anchoredPosition = new Vector2(background.GetComponent<RectTransform>().anchoredPosition.x, -250f);
+            content.sizeDelta = new Vector2(background.GetComponent<RectTransform>().sizeDelta.x, 0f);
 
-            sequence.Append(background.DOFade(1f, animationTime));
-            sequence.Join(background.GetComponent<RectTransform>().DOAnchorPosY(16.75f, animationTime));
-            sequence.Join(background.GetComponent<RectTransform>().DOSizeDelta(new Vector2(background.GetComponent<RectTransform>().sizeDelta.x, 33.5f), animationTime));
-            sequence.Join(content.DOFade(1f, animationTime));
-            sequence.Join(content.GetComponent<RectTransform>().DOAnchorPosY(0f, animationTime));
-            sequence.Join(content.GetComponent<RectTransform>().DOSizeDelta(new Vector2(background.GetComponent<RectTransform>().sizeDelta.x, 0f), animationTime));
-
-            sequence.OnComplete(() =>
-            {
-                LayoutRebuilder.ForceRebuildLayoutImmediate(content.GetComponent<RectTransform>());
-            });
+            sequence.Append(background.GetComponent<CanvasGroup>().DOFade(1f, animationTime));
+            sequence.Join(background.DOAnchorPosY(16.75f, animationTime));
+            sequence.Join(background.DOSizeDelta(new Vector2(background.GetComponent<RectTransform>().sizeDelta.x, 33.5f), animationTime));
+            sequence.Join(content.GetComponent<CanvasGroup>().DOFade(1f, animationTime));
+            sequence.Join(content.DOAnchorPosY(0f, animationTime));
+            sequence.Join(content.DOSizeDelta(new Vector2(background.GetComponent<RectTransform>().sizeDelta.x, 0f), animationTime));
         }
         else
         {
-            sequence.Append(background.DOFade(0f, animationTime));
-            sequence.Join(content.DOFade(0f, animationTime));
-            sequence.Join(content.GetComponent<RectTransform>().DOScale(new Vector2(0.9f, 0.95f), animationTime));
+            sequence.Append(background.GetComponent<CanvasGroup>().DOFade(0f, animationTime));
+            sequence.Join(content.GetComponent<CanvasGroup>().DOFade(0f, animationTime));
+            sequence.Join(content.DOScale(new Vector2(0.9f, 0.95f), animationTime));
 
             sequence.OnComplete(() =>
             {
@@ -84,17 +76,7 @@ public class UIAnimation : MonoBehaviour
     
     private void Awake()
     {
-        background = transform.Find("Background").GetComponent<CanvasGroup>();
-    }
-
-    private void Start()
-    {
-        
-    }
-
-    private void Update()
-    {
-        
+        background = transform.Find("Background").GetComponent<RectTransform>();
     }
 
     #endregion
